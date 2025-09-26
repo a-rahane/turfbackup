736141fdb93c1bec289e16d4a245cae62f31f1ec
Aadita Rahane <91989699+a-rahane@users.noreply.github.com>
2025-09-27 02:25:28 +0530

    Update config.php

--- config.php
+++ config.php
@@ -11,6 +11,6 @@ try {
     $pdo = new PDO($dsn, $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+    die("Database connection failed: " . $e->getMessage());
 }
 ?>
